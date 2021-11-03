e10e4896e0516b3b499a2093b384b46c4b395b1a
Hansanghyeon <999@hyeon.pro>
2021-11-03 16:25:54 +0900

    :sparkles: init alpine js

--- src/resources/scripts/app.js
+++ src/resources/scripts/app.js
@@ -1,8 +1,22 @@
-/**
- * External Dependencies
- */
 import 'jquery';
+import Alpine from 'alpinejs';
+import intersect from '@alpinejs/intersect';
 
-$(() => {
-  // console.log('Hello world');
+Alpine.plugin(intersect);
+
+import Router from './util/router';
+
+const routes = new Router({
+  // All pages
+  common: {
+    containerInit() {},
+    init() {},
+    finalize() {},
+  },
+});
+
+// Load Events
+jQuery(document).ready(() => {
+  routes.loadEvents();
+  Alpine.start();
 });

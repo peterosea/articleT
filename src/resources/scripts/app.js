00be8fa9d1dd36ff511cf6d643c563b755bcd7f9
Hansanghyeon <999@hyeon.pro>
2021-11-05 09:48:46 +0900

    :sparkles: gnb shrink & headroom scripting
    Resolves: beeclover/tbstory#12

--- src/resources/scripts/app.js
+++ src/resources/scripts/app.js
@@ -4,13 +4,20 @@ import intersect from '@alpinejs/intersect';
 
 Alpine.plugin(intersect);
 
+// componments
+import header from './components/header';
+import shrink from './components/shrink';
+// Routers
 import Router from './util/router';
 
 const routes = new Router({
   // All pages
   common: {
     containerInit() {},
-    init() {},
+    init() {
+      header();
+      shrink();
+    },
     finalize() {},
   },
 });

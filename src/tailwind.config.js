67cfaaf607d8ee6e54b129b099844508c3354e98
Hansanghyeon <999@hyeon.pro>
2021-11-13 01:10:46 +0900

    :lipstick: container 최대 크기 조정

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -33,13 +33,12 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    // screen: {
-    //   'sm':'640px',
-    //   'md':'768px',
-    //   'lg':'1024px',
-    //   'xl':'1280px',
-    //   '2xl':'1536px',
-    // },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
     extend: {
       padding: {
         'container-x': 'var(--container_px)',
@@ -99,5 +98,6 @@ module.exports = {
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
   ],
 };

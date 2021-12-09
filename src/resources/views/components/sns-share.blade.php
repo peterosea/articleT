8eee8154f65e48b4a5a3c8e5f114e8f69abe2ab5
Hansanghyeon <999@hyeon.pro>
2021-12-09 13:46:56 +0900

    :bug: sns & list 링크 버튼 기능 동작 하지않음
    Resolves: beeclover/articleT#61

--- src/resources/views/components/sns-share.blade.php
+++ src/resources/views/components/sns-share.blade.php
@@ -1,12 +1,14 @@
+@if ($script)
   <script>
-  function share_fb(url) {
-    window.open('https://www.facebook.com/sharer/sharer.php?u='+url,'facebook-share-dialog',"width=626, height=436")
-  }
-  function new_window(url) {
-    window.open(url,'_blank', "width=626, height=436")
-  }
+    function share_fb(url) {
+      window.open('https://www.facebook.com/sharer/sharer.php?u='+url,'facebook-share-dialog',"width=626, height=436")
+    }
+    function new_window(url) {
+      window.open(url,'_blank', "width=626, height=436")
+    }
   </script>
+@endif
 
-<div class="sns flex gap-x-[16px] text-[24px]">
+<div class="sns flex gap-x-[16px] text-[24px] justify-center">
   {!! do_shortcode('[Sassy_Social_Share]') !!}
 </div>

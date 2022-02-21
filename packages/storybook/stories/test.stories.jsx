c39238ffe6e3ecd8afebe8c18fe1e56054430220
999 <999@hyeon.pro>
2022-02-21 12:44:18 +0900

    :construction: monorepo (beeclover/articleT#106)

---
+++ packages/storybook/stories/test.stories.jsx
@@ -0,0 +1,10 @@
+import { Button } from 'ui'
+
+export default {
+  title: 'test/button',
+  component: Button,
+};
+
+export const defaultButton = () => {
+  return <Button>default 버튼</Button>;
+};
